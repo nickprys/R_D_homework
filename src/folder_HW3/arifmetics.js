7ea7c35b9bf678ac222d60318d5a21d66610b176
Myko_Prys <nickprys@gmail.com>
2025-02-03 17:57:14 +0100

    dz3

---
+++ src/folder_HW3/arifmetics.js
@@ -0,0 +1,12 @@
+function ariphmetics() {
+    const a = 8;
+    const b = 'x';
+    let c;
+    c = a + b;
+    const d1 = 8;
+    const d2 = 6;
+    let d3 = d1/d2;
+    console.log(c, d3);
+}
+
+ariphmetics();
